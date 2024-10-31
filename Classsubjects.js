2c1d5bb9e65f6af745a258cf6405d2e41af98b9e
AshleyValdez061105 <ashleyvaldez061105@gmail.com>
2024-10-31 09:38:26 +0800

    Classsubjects.js

---
+++ Classsubjects.js
@@ -0,0 +1,12 @@
+// Collect the following user information:
+let subjectTitle = "Data structure and algorithms ";
+let classSchedule = "M,T,W,F, 8:-9:30AM, 11-12:00AM";
+let classroom = "Comlab B";
+let classInstructor = "Cliff owen pascua";
+let studentName = "Ashley Valdez";
+
+// Use variables to store this information and log it to the console with a specific string format
+
+console.log("{Ashley Valdez} is currently enrolled in {Data structure and algorithms}
+with a class schedule of {M,T,W,F, 8:-9:30AM, 11-12:00AM} at room {Comlab B}. 
+the instructor for the subject is {Cliff owen pascua}");
