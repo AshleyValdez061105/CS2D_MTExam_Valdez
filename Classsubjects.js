471808cf3b8cfc67db7b12ff87a4e2f9cb3c9a2c
AshleyValdez061105 <ashleyvaldez061105@gmail.com>
2024-11-05 08:05:54 +0800

    Classsubjects.js

--- Classsubjects.js
+++ Classsubjects.js
@@ -1,12 +1,12 @@
 // Collect the following user information:
-let subjectTitle = "Data structure and algorithms ";
-let classSchedule = "M,T,T,F, 8-9:00AM, 11-12:00AM";
-let classroom = "Comlab B";
-let classInstructor = "Cliff owen pascua";
-let studentName = "Ashley Valdez";
+let subjectTitle = "Data structure and algorithms ";   // This stores the title of the class
+let classSchedule = "M,T,T,F, 8-9:00AM, 11-12:00AM";   // This stores the class schedule
+let classroom = "Comlab B";      // This stores the classroom location
+let classInstructor = "Cliff owen pascua";  // This stores the name of the instructor
+let studentName = "Ashley Valdez";   // This stores the name of the student
 
 // Use variables to store this information and log it to the console with a specific string format
 
 console.log("{Ashley Valdez} is currently enrolled in {Data structure and algorithms}
 with a class schedule of {M,T,T,F, 8-9:00AM, 11-12:00AM} at room {Comlab B}. 
-the instructor for the subject is {Cliff owen pascua}");
+the instructor for the subject is {Cliff owen pascua}");  // This line uses the  console.log()  function to display the student information to the console. It constructs a string that includes the student's name, class title, schedule, classroom, and instructor's name
